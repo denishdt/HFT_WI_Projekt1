2cb4f6cdee0615e343987c3618fdd4fe9f66626c
Justin Waidmann <31waju1bwi@hft-stuttgart.de>
2023-11-16 23:31:56 +0100

    new sysout.

--- HFT_WI-Projekt_Code/src/de/hft_stuttgart/buysmartenterprise/main/Main.java
+++ HFT_WI-Projekt_Code/src/de/hft_stuttgart/buysmartenterprise/main/Main.java
@@ -7,5 +7,6 @@ public class Main {
 	public static void main(String [] args) {
 		DBAccess.dbAccess();
 		System.out.println("Hallo, wir sind BuySmart Enterprise!");
+		System.out.println("Hallo, Justin war halbe st. vor seinem geburtstag hier :(");
 	}
 }
