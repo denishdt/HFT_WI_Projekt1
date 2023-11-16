2aa32028a4a56b09d52894e80f91640412614edf
denishdt <denis.heidt@web.de>
2023-11-16 22:48:30 +0100

    First Commit

---
+++ HFT_WI-Projekt_Code/src/de/hft_stuttgart/buysmartenterprise/main/Main.java
@@ -0,0 +1,8 @@
+package de.hft_stuttgart.buysmartenterprise.main;
+
+public class Main {
+
+	public static void main(String [] args) {
+		System.out.println("Hallo, wir sind BuySmart Enterprise!");
+	}
+}
