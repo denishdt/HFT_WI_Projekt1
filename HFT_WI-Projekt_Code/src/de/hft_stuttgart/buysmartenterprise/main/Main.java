c1113c9f876c602d40e89f4a0673dc71787a14a5
Justin Waidmann <31waju1bwi@hft-stuttgart.de>
2023-11-24 15:57:53 +0100

    test kommentar

--- HFT_WI-Projekt_Code/src/de/hft_stuttgart/buysmartenterprise/main/Main.java
+++ HFT_WI-Projekt_Code/src/de/hft_stuttgart/buysmartenterprise/main/Main.java
@@ -7,7 +7,7 @@ public class Main {
 	
 	public static void main(String [] args) {
 		System.out.println("HighSpeed Procurement by BuySmart Enterprise");
-		//yarak
+		//yarak // selber
 		GUI_Login openLogin = new GUI_Login();	
 	}
 	
