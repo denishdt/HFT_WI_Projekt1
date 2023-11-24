f5a4b6d0310b9fb4457074d80142d9881b1744b0
Altin1610 <hiimaltin@gmail.com>
2023-11-24 15:28:18 +0100

    Neue Bestellung gleich gemacht

--- HFT_WI-Projekt_Code/src/de/hft_stuttgart/buysmartenterprise/gui/GUI_new_order.java
+++ HFT_WI-Projekt_Code/src/de/hft_stuttgart/buysmartenterprise/gui/GUI_new_order.java
@@ -57,62 +57,15 @@ public class GUI_new_order {
 	private void initialize() {
 		frame = new JFrame("Highspeed Procurement");
 		frame.getContentPane().setBackground(new Color(255, 255, 255));
-		frame.setBounds(100, 100, 800, 300);
+		frame.setBounds(100, 100, 700, 300);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.getContentPane().setLayout(null);
 		
 		JPanel panel = new JPanel();
 		panel.setBackground(new Color(255, 255, 255));
-		panel.setBounds(10, 23, 766, 230);
+		panel.setBounds(10, 35, 664, 226);
 		frame.getContentPane().add(panel);
 		panel.setLayout(null);
-		
-		JPanel panel_1 = new JPanel();
-		panel_1.setBackground(new Color(50, 205, 50));
-		panel_1.setBounds(229, 27, 206, 32);
-		panel.add(panel_1);
-		panel_1.setLayout(null);
-		
-		JLabel lblNewLabel_2 = new JLabel("Bestellung erstellen");
-		lblNewLabel_2.setFont(new Font("Tahoma", Font.PLAIN, 12));
-		lblNewLabel_2.setForeground(new Color(255, 255, 255));
-		lblNewLabel_2.setBackground(new Color(255, 255, 255));
-		lblNewLabel_2.setBounds(0, 10, 121, 12);
-		panel_1.add(lblNewLabel_2);
-		
-		JPanel panel_2 = new JPanel();
-		panel_2.setBackground(new Color(0, 0, 0));
-		panel_2.setBounds(10, 27, 221, 32);
-		panel.add(panel_2);
-		panel_2.setLayout(null);
-		
-		JLabel lblNewLabel_1 = new JLabel("BuySmartEnterprise");
-		lblNewLabel_1.setFont(new Font("Tahoma", Font.PLAIN, 17));
-		lblNewLabel_1.setForeground(new Color(128, 128, 128));
-		lblNewLabel_1.setBackground(new Color(192, 192, 192));
-		lblNewLabel_1.setBounds(10, 0, 159, 32);
-		panel_2.add(lblNewLabel_1);
-		
-		JPanel panel_3 = new JPanel();
-		panel_3.setBackground(new Color(0, 0, 0));
-		panel_3.setBounds(433, 28, 323, 31);
-		panel.add(panel_3);
-		panel_3.setLayout(null);
-		
-		JLabel lblNewLabel_3 = new JLabel("Log Out");
-		lblNewLabel_3.setForeground(new Color(128, 128, 128));
-		lblNewLabel_3.setBounds(257, 10, 56, 13);
-		panel_3.add(lblNewLabel_3);
-		
-		JLabel lblNewLabel_4 = new JLabel("Profile");
-		lblNewLabel_4.setForeground(new Color(128, 128, 128));
-		lblNewLabel_4.setBounds(201, 10, 46, 13);
-		panel_3.add(lblNewLabel_4);
-		
-		JLabel lblNewLabel_5 = new JLabel("Settings");
-		lblNewLabel_5.setForeground(new Color(128, 128, 128));
-		lblNewLabel_5.setBounds(135, 10, 56, 13);
-		panel_3.add(lblNewLabel_5);
 		
 		JPanel panel_4 = new JPanel();
 		panel_4.setBackground(new Color(255, 255, 255));
@@ -126,48 +79,35 @@ public class GUI_new_order {
 		lblNewLabel_9.setBounds(0, 0, 221, 32);
 		panel_4.add(lblNewLabel_9);
 		
-		JPanel panel_5 = new JPanel();
-		panel_5.setBackground(new Color(255, 255, 255));
-		panel_5.setBounds(325, 66, 143, 32);
-		panel.add(panel_5);
-		panel_5.setLayout(null);
-		
-		JLabel lblNewLabel_8 = new JLabel("Menge festlegen");
-		lblNewLabel_8.setBackground(new Color(255, 255, 255));
-		lblNewLabel_8.setFont(new Font("Tahoma", Font.PLAIN, 16));
-		lblNewLabel_8.setHorizontalAlignment(SwingConstants.CENTER);
-		lblNewLabel_8.setBounds(0, 0, 143, 32);
-		panel_5.add(lblNewLabel_8);
-		
 		JPanel panel_6 = new JPanel();
 		panel_6.setBackground(new Color(255, 255, 255));
-		panel_6.setBounds(511, 66, 162, 32);
+		panel_6.setBounds(427, 66, 162, 32);
 		panel.add(panel_6);
 		panel_6.setLayout(null);
 		
 		JLabel lblNewLabel_7 = new JLabel("Lieferanten auswählen");
+		lblNewLabel_7.setBounds(0, 0, 162, 32);
+		panel_6.add(lblNewLabel_7);
 		lblNewLabel_7.setBackground(new Color(255, 255, 255));
 		lblNewLabel_7.setFont(new Font("Tahoma", Font.PLAIN, 14));
 		lblNewLabel_7.setHorizontalAlignment(SwingConstants.CENTER);
-		lblNewLabel_7.setBounds(0, 0, 162, 32);
-		panel_6.add(lblNewLabel_7);
 		
 		JPanel panel_7 = new JPanel();
 		panel_7.setBackground(new Color(255, 255, 255));
-		panel_7.setBounds(700, 66, 56, 32);
+		panel_7.setBounds(599, 66, 56, 32);
 		panel.add(panel_7);
 		panel_7.setLayout(null);
 		
 		JLabel lblNewLabel_6 = new JLabel("Preis:");
+		lblNewLabel_6.setBounds(0, 0, 56, 32);
+		panel_7.add(lblNewLabel_6);
 		lblNewLabel_6.setHorizontalAlignment(SwingConstants.CENTER);
 		lblNewLabel_6.setFont(new Font("Tahoma", Font.PLAIN, 16));
 		lblNewLabel_6.setForeground(new Color(0, 0, 0));
 		lblNewLabel_6.setBackground(new Color(255, 255, 255));
-		lblNewLabel_6.setBounds(0, 0, 56, 32);
-		panel_7.add(lblNewLabel_6);
 		
 		textField = new JTextField();
-		textField.setBounds(420, 108, 48, 32);
+		textField.setBounds(334, 108, 48, 32);
 		panel.add(textField);
 		textField.setColumns(10);
 		
@@ -178,18 +118,18 @@ public class GUI_new_order {
 			public void actionPerformed(ActionEvent e) {
 			}
 		});
-		btnNewButton.setBounds(554, 188, 96, 32);
+		btnNewButton.setBounds(448, 187, 96, 32);
 		panel.add(btnNewButton);
 		
 		JButton btnNewButton_1 = new JButton("Bestellung abschließen");
 		btnNewButton_1.setBackground(new Color(0, 0, 128));
 		btnNewButton_1.setForeground(new Color(255, 255, 255));
-		btnNewButton_1.setBounds(660, 188, 96, 32);
+		btnNewButton_1.setBounds(559, 187, 96, 32);
 		panel.add(btnNewButton_1);
 		
 		textField_1 = new JTextField();
 		textField_1.setEditable(false);
-		textField_1.setBounds(700, 108, 56, 32);
+		textField_1.setBounds(599, 101, 56, 32);
 		panel.add(textField_1);
 		textField_1.setColumns(10);
 		
@@ -201,16 +141,55 @@ public class GUI_new_order {
 		comboBox.setBackground(new Color(255, 255, 255));
 		
 		JComboBox comboBox_1 = new JComboBox();
-		comboBox_1.setBounds(511, 108, 162, 20);
+		comboBox_1.setBounds(427, 109, 162, 20);
 		panel.add(comboBox_1);
 		
-		JLabel lblNewLabel = new JLabel("BuySmart Enterprise");
-		lblNewLabel.setBounds(0, 0, 129, 13);
-		frame.getContentPane().add(lblNewLabel);
+		JLabel lblNewLabel_8 = new JLabel("Menge festlegen");
+		lblNewLabel_8.setBounds(248, 66, 143, 32);
+		panel.add(lblNewLabel_8);
+		lblNewLabel_8.setBackground(new Color(255, 255, 255));
+		lblNewLabel_8.setFont(new Font("Tahoma", Font.PLAIN, 16));
+		lblNewLabel_8.setHorizontalAlignment(SwingConstants.CENTER);
 		
-		JSeparator separator = new JSeparator();
-		separator.setBounds(10, 11, 808, 13);
-		frame.getContentPane().add(separator);
+		JPanel panel_1 = new JPanel();
+		panel_1.setBackground(Color.BLACK);
+		panel_1.setBounds(0, 0, 683, 33);
+		frame.getContentPane().add(panel_1);
+		
+		JLabel lblNewLabel = new JLabel("BuySmart Enterprise   ");
+		lblNewLabel.setForeground(Color.LIGHT_GRAY);
+		lblNewLabel.setFont(new Font("Tahoma", Font.PLAIN, 14));
+		panel_1.add(lblNewLabel);
+		
+		JLabel lblNewLabel_1 = new JLabel("Neue Bestellung");
+		lblNewLabel_1.setOpaque(true);
+		lblNewLabel_1.setForeground(Color.WHITE);
+		lblNewLabel_1.setFont(new Font("Tahoma", Font.PLAIN, 13));
+		lblNewLabel_1.setBackground(Color.GRAY);
+		panel_1.add(lblNewLabel_1);
+		
+		JLabel lblNewLabel_2 = new JLabel("                                           ");
+		panel_1.add(lblNewLabel_2);
+		
+		JButton btnNewButton_2 = new JButton("Shop");
+		btnNewButton_2.setForeground(Color.WHITE);
+		btnNewButton_2.setBackground(Color.BLACK);
+		panel_1.add(btnNewButton_2);
+		
+		JButton btnNewButton_1_1 = new JButton("Settings");
+		btnNewButton_1_1.setForeground(Color.WHITE);
+		btnNewButton_1_1.setBackground(Color.BLACK);
+		panel_1.add(btnNewButton_1_1);
+		
+		JButton btnNewButton_2_1 = new JButton("Profile");
+		btnNewButton_2_1.setForeground(Color.WHITE);
+		btnNewButton_2_1.setBackground(Color.BLACK);
+		panel_1.add(btnNewButton_2_1);
+		
+		JButton btnNewButton_3 = new JButton("Log Out");
+		btnNewButton_3.setForeground(Color.WHITE);
+		btnNewButton_3.setBackground(Color.BLACK);
+		panel_1.add(btnNewButton_3);
 		
 		frame.addWindowListener(new WindowAdapter() {
 			@Override
