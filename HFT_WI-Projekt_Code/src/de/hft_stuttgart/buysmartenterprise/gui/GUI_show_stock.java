62096df3cabd53e019de7b664fddadc919e7b623
elham437 <ichbinelham@gmail.com>
2024-01-10 22:20:06 +0100

    textfield

--- HFT_WI-Projekt_Code/src/de/hft_stuttgart/buysmartenterprise/gui/GUI_show_stock.java
+++ HFT_WI-Projekt_Code/src/de/hft_stuttgart/buysmartenterprise/gui/GUI_show_stock.java
@@ -168,6 +168,7 @@ public class GUI_show_stock {
 		frame.getContentPane().add(list);
 		
 		txtBestandDerEinzelteile = new JTextField();
+		txtBestandDerEinzelteile.setEditable(false);
 		txtBestandDerEinzelteile.setForeground(new Color(0, 124, 128));
 		txtBestandDerEinzelteile.setFont(new Font("Tahoma", Font.BOLD, 12));
 		txtBestandDerEinzelteile.setHorizontalAlignment(SwingConstants.CENTER);
@@ -177,6 +178,7 @@ public class GUI_show_stock {
 		txtBestandDerEinzelteile.setColumns(10);
 		
 		textField = new JTextField();
+		textField.setEditable(false);
 		textField.setText("Bestand");
 		textField.setHorizontalAlignment(SwingConstants.CENTER);
 		textField.setForeground(new Color(0, 124, 128));
