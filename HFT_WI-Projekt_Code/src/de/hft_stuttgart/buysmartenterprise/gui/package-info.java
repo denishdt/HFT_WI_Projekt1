be8cd15962000ed395c4c278a228eab1fc28a805
elham437 <ichbinelham@gmail.com>
2024-01-11 21:51:39 +0100

    doc test

---
+++ HFT_WI-Projekt_Code/src/de/hft_stuttgart/buysmartenterprise/gui/package-info.java
@@ -0,0 +1,19 @@
+/**
+ * Das Paket de.hft_stuttgart.buysmartenterprise.gui enthält Klassen für den...
+ * 
+ * Diese Paket enthält folgende Klassen:
+ * -GUI_login: Hier kann man sich einloggen
+ * -GUI_dashboard: Man kann auf jede GUI zugreifen (Homepage)
+ * -GUI_new_epmloyee: Hier kann man neue Mitarbeiter hinzufügen 
+ * -GUI_new_order: Zum neue Bestellungen aufgeben
+ * -GUI_new_supplier: Einen neuen Lieferanten hinzufügen
+ * -GUI_order_status: Zeigt den Bestellstatus
+ * -GUI_profile: Benutzername und/oder Passwort ändern
+ * -GUI_show_stock: Zeigt den Warenbestand an
+ * -GUI_show_supplier: Zeigt die Lieferanten an
+ * -GUI_standard_quantity: Hier kann man die Mindest- und Standardmenge festlegen
+ * -GUI_standard_supplier: Hier kann man den Standardlieferanten festlegen
+ */
+
+
+package de.hft_stuttgart.buysmartenterprise.gui;
---
+++ HFT_WI-Projekt_Code/src/de/hft_stuttgart/buysmartenterprise/main/package-info.java
@@ -0,0 +1,9 @@
+/**
+ * Das Paket de.hft_stuttgart.buysmartenterprise.main enthält eine Klasse damit das Programm laufen kann.
+ * 
+ * Diese Paket enthält folgende Klassen:
+ * - Main: Startet das Programm und öffnet "GUI_login"
+ */
+
+
+package de.hft_stuttgart.buysmartenterprise.main;
